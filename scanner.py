e6b4ca04e1495e17f7da6a266cd872902ef0db2f
2heoh <sergey.lobin@gmail.com>
2021-03-10 17:37:41 +0300

    Fixed concatenation

--- scanner.py
+++ scanner.py
@@ -29,7 +29,7 @@ class AlertScanner:
             transport.initialize()
             transport.transmit_requiring_acknowledgement(self.ADMIN_ON_CALL_DEVICE,
                                                          "New inbound critical patient: " +
-                                                         patient.get_transport_id())
+                                                         str(patient.get_transport_id()))
             self.critical_patient_notifications_sent.append(patient.get_transport_id())
         except RuntimeError:
             print("Failed attempt to use pager system to device " + self.ADMIN_ON_CALL_DEVICE)
